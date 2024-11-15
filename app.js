fccd9e01afd84c3e44023dc638ce4091eaf02b4b
Carly Plumridge <c-plumridge@hotmail.com>
2024-11-15 21:09:40 +0000

    Update app.js
    Added Carly's introduction directly to the main branch

--- app.js
+++ app.js
@@ -6,3 +6,7 @@
 // Ameenah:
 // Hi, my name's Ameenah, I'm a London girl and I enjoy game and web development and music.
 // I enjoy creative things, so that's why I love those things, and also why I'm here.
+
+//Hi, I’m Carly! I live in Peterborough with my husband, 
+//two children, and our cat. In my free time I love to hike, bake sweet treats, 
+//and tackle fun crafting projects.
