44e1c420ae9923f1cb223b98fea1ab887423e181
Carly Plumridge <c-plumridge@hotmail.com>
2024-11-15 20:09:10 +0000

    Update app.js
    Carly Introduction added

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 //Hello, my name is Filsan. I live in Leicester and my favourite sport is
 //horse riding.
+
+//Hi, I’m Carly! I live in Peterborough with my husband, 
+//two children, and our cat. In my free time I love to hike, bake sweet treats, 
+//and tackle fun crafting projects.
