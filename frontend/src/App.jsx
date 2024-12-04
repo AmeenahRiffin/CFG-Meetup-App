5d3f29d32e47670c45db6969d49dcefc4b5ce36c
somholuk <filsanhdmohamed@gmail.com>
2024-12-04 09:18:44 +0000

    added UpcomingEvents.jsx and UpcomingEvents.css to the UpcomingEvents folder in the webpages folder that is in the pages folder.Furthermore, I have commented out the css code in the App.css and the index.css to prevent style collisions.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from "./components/Webpages/HomePage";
-import AboutPage from "./components/Webpages/AboutPage";
-import RegisterPage from "./components/Webpages/RegisterPage";
-import LoginPage from "./components/Webpages/LoginPage";
-import './components/Styles/homepage.css'
-import './components/Styles/aboutpage.css'
-import './components/Styles/registerpage.css'
-import './components/Styles/loginpage.css'
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import HomePage from "./components/Webpages/HomePage";
+// import AboutPage from "./components/Webpages/AboutPage";
+// import RegisterPage from "./components/Webpages/RegisterPage";
+// import LoginPage from "./components/Webpages/LoginPage";
+// import './components/Styles/homepage.css'
+// import './components/Styles/aboutpage.css'
+// import './components/Styles/registerpage.css'
+// import './components/Styles/loginpage.css'
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/about" element={<AboutPage />} />
+//           <Route path="/register" element={<RegisterPage />} />
+//           <Route path="/login" element={<LoginPage />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import UpcomingEvents from "./pages/webpages/UpcomingEvents/UpcomingEvents"; // Corrected path to UpcomingEvents component
+
+function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </Router>
+    <div className="App">
+      <UpcomingEvents /> {/* Render the UpcomingEvents component */}
     </div>
   );
-};
-
+}
 
 export default App;
